b5262780477d83ba51199dd770fef4980f2340c5
kaz-yos <kazky@mac.com>
2014-05-02 23:02:11 -0400

    added definitions of the game board. added position functions

--- src/othe/model.clj
+++ src/othe/model.clj
@@ -1,2 +1,30 @@
 (ns othe.model)
 
+;; Size of each dimension
+(def b-size 8)
+
+;; Position of the first cell
+(def first-pos 0)
+
+;; Position of the last cell
+(def last-pos (* b-size b-size))
+
+;; List of all possible positions
+(def all-pos (range first-pos last-pos))
+
+;; Column indexes
+(def first-col 0)
+(def last-col b-size)
+
+;; Row indexes
+(def first-row 0)
+(def last-row b-size)
+
+;; Position and index mutual conversion
+(defn col-from-pos [pos] (mod pos b-size))
+(defn row-from-pos [pos] (quot pos b-size))
+(defn pos-from-rowcol [r c]
+  (+ (* r b-size) c))
+
+;; Collection of all possible directions
+(def dirs #{:n :ne :e :se :s :sw :w :nw})

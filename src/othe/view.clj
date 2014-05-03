5382e4f391228df20fa48a9bed63e7061ea57aad
kaz-yos <kazky@mac.com>
2014-05-02 23:14:50 -0400

    column namer added

--- src/othe/view.clj
+++ src/othe/view.clj
@@ -2,3 +2,15 @@
   (:use
    othe.model
    [clojure.string :only (join)]))
+
+
+;; Names for columns
+(def code-a 97)
+(def code-curly 123)
+(def col-headers
+  (take b-size
+        (map (comp str char)
+             (range code-a code-curly))))
+(def col-header-str
+  (str " " (join " " col-headers)))
+
